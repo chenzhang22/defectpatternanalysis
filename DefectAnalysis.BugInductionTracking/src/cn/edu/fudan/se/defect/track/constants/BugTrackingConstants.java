34c56d1bf443900822d2e4b926f5299a6072f5bb
Lotay Liu <lotay.liu@gmail.com>
2014-10-14 11:18:45 +0800

    add new constants

--- DefectAnalysis.BugInductionTracking/src/cn/edu/fudan/se/defect/track/constants/BugTrackingConstants.java
+++ DefectAnalysis.BugInductionTracking/src/cn/edu/fudan/se/defect/track/constants/BugTrackingConstants.java
@@ -1,16 +1,18 @@
 /**
  * 
  */
-package cn.edu.fudan.se.defect.track.git.constants;
+package cn.edu.fudan.se.defect.track.constants;
 
 import java.util.regex.Pattern;
 
 /**
  * @author Lotay
- *
+ * 
  */
 public class BugTrackingConstants {
 	public static final String ECLIPSE_CORE_GIT_REPO_PATH = "E:/Codes/libs/Eclipse/eclipse.jdt.core/.git";
 	public static final String FILTED_SOURCEFILE_STRING = "^(.*)test(.*)$";
-	public static final Pattern FILTED_SOURCEFILE_PATTERN = Pattern.compile(FILTED_SOURCEFILE_STRING, Pattern.CASE_INSENSITIVE);
+	public static final Pattern FILTED_SOURCEFILE_PATTERN = Pattern.compile(
+			FILTED_SOURCEFILE_STRING, Pattern.CASE_INSENSITIVE);
+	public static final String BUG_FIXED_STATUS[] = { "RESOLVED", "VERIFIED" };
 }
