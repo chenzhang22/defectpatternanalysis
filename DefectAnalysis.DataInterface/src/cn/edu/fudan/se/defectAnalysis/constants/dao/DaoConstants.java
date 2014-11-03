3a87b6f0d4cda82b170c995122a3e3adba5030d5
Lotay Liu <lotay.liu@gmail.com>
2014-11-03 09:45:31 +0800

    change the path.

--- DefectAnalysis.DataInterface/src/cn/edu/fudan/se/defectAnalysis/constants/dao/DaoConstants.java
+++ DefectAnalysis.DataInterface/src/cn/edu/fudan/se/defectAnalysis/constants/dao/DaoConstants.java
@@ -8,6 +8,6 @@ package cn.edu.fudan.se.defectAnalysis.constants.dao;
  * 
  */
 public final class DaoConstants {
-	public static final String ECLIPSE_HIBERNATE_LOCATION_PATH = "。/DefectAnalysis.DataInterface/conf/hibernate_eclipse.cfg.xml";
-	public static final String TOMCAT_HIBERNATE_LOCATION_PATH = "。/DefectAnalysis.DataInterface/conf/hibernate_tomcat.cfg.xml";
+	public static final String ECLIPSE_HIBERNATE_LOCATION_PATH = ".。/DefectAnalysis.DataInterface/conf/hibernate_eclipse.cfg.xml";
+	public static final String TOMCAT_HIBERNATE_LOCATION_PATH = ".。/DefectAnalysis.DataInterface/conf/hibernate_tomcat.cfg.xml";
 }
