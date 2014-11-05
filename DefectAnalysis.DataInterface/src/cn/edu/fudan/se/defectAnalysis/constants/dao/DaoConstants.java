c53e0d85c11d93b72896c5a8f64ce971e71984b9
Lotay Liu <lotay.liu@gmail.com>
2014-11-05 20:44:42 +0800

    add eclipse ui

--- DefectAnalysis.DataInterface/src/cn/edu/fudan/se/defectAnalysis/constants/dao/DaoConstants.java
+++ DefectAnalysis.DataInterface/src/cn/edu/fudan/se/defectAnalysis/constants/dao/DaoConstants.java
@@ -11,4 +11,6 @@ public final class DaoConstants {
 	public static final String ECLIPSE_CORE_HIBERNATE_LOCATION_PATH = "../DefectAnalysis.DataInterface/conf/hibernate_eclipse_core.cfg.xml";
 	public static final String TOMCAT_HIBERNATE_LOCATION_PATH = "../DefectAnalysis.DataInterface/conf/hibernate_tomcat.cfg.xml";
 	public static final String ECLIPSE_UI_HIBERNATE_LOCATION_PATH = "../DefectAnalysis.DataInterface/conf/hibernate_eclipse_ui.cfg.xml";
+	public static final String ECLIPSE_DEBUG_HIBERNATE_LOCATION_PATH = "../DefectAnalysis.DataInterface/conf/hibernate_eclipse_debug.cfg.xml";
+
 }
