56f449d770e39e5f28c590fa7216837fe5b06d31
Lotay Liu <lotay.liu@gmail.com>
2014-11-05 12:50:51 +0800

    change the interface

--- DefectAnalysis.GitDataExtraction/src/cn/edu/fudan/se/git/explore/constants/GitExploreConstants.java
+++ DefectAnalysis.GitDataExtraction/src/cn/edu/fudan/se/git/explore/constants/GitExploreConstants.java
@@ -5,6 +5,8 @@ package cn.edu.fudan.se.git.explore.constants;
 
 import java.util.regex.Pattern;
 
+import cn.edu.fudan.se.defectAnalysis.constants.dao.DaoConstants;
+
 /**
  * @author Lotay
  *
@@ -15,14 +17,12 @@ public class GitExploreConstants {
 	public static final String FILTED_SOURCEFILE_STRING = "^(.*)test(.*)$";
 	public static final Pattern FILTED_SOURCEFILE_PATTERN = Pattern.compile(
 			FILTED_SOURCEFILE_STRING, Pattern.CASE_INSENSITIVE);
-	public static final String ECLIPSE_HIBERNATE_CONF_PATH = "conf/hibernate_eclipse.cfg.xml";
-	public static final String TOMCAT_HIBERNATE_CONF_PATH = "conf/hibernate_tomcat.cfg.xml";
 
 	public static final String SOURCE_FILE_SUFFIX = ".java";
 	public static final String ECLIPSE_BUG_PATTERN_MATCH = "(.*\\[[0-9]+\\].*)|([0-9]+)|(.*bug.*[0-9]+.*)|(.*fix.*[0-9]+.*)|(.*patch.*[0-9]+.*)|([0-9]+.*)|(.*[0-9]+)|(.*#[0-9].*)";
 	public static final String TOMCAT_BUG_PATTERN_MATCH = "https.*show_bug.*id=[0-9]+";
 
-	public static final String HIBERNATE_CONF_PATH = TOMCAT_HIBERNATE_CONF_PATH;
+	public static final String HIBERNATE_CONF_PATH = DaoConstants.TOMCAT_HIBERNATE_LOCATION_PATH;
 	public static final String BUG_PATTERN_MATCH = TOMCAT_BUG_PATTERN_MATCH;
 	public static final String GIT_REPO_PATH = TOMCAT_GIT_REPO_PATH;
 
