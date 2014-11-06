682497fec64d44ae5406e9de3a9c7f441579ff3e
Lotay Liu <lotay.liu@gmail.com>
2014-11-06 16:14:08 +0800

    change the constants

--- DefectAnalysis.BugInductionTracking/src/cn/edu/fudan/se/defect/track/constants/BugTrackingConstants.java
+++ DefectAnalysis.BugInductionTracking/src/cn/edu/fudan/se/defect/track/constants/BugTrackingConstants.java
@@ -13,6 +13,8 @@ import cn.edu.fudan.se.defectAnalysis.constants.dao.DaoConstants;
  */
 public class BugTrackingConstants {
 	public static final String ECLIPSE_CORE_GIT_REPO_PATH = "E:/Codes/libs/Eclipse/eclipse.jdt.core/.git";
+	public static final String ECLIPSE_UI_GIT_REPO_PATH = "E:/Codes/libs/Eclipse/eclipse.jdt.ui/.git";
+	
 	public static final String TOMCAT_GIT_REPO_PATH ="E:/Codes/libs/Apache/tomcat/.git";
 	public static final String FILTED_SOURCEFILE_STRING = "^(.*)test(.*)$";
 	public static final Pattern FILTED_SOURCEFILE_PATTERN = Pattern.compile(
@@ -24,6 +26,6 @@ public class BugTrackingConstants {
 	public static final String CODE_DELETED = "CODE_DELETED";
 	
 	//Need changes for different git repository....
-	public static final String GIT_REPO_PATH = TOMCAT_GIT_REPO_PATH;
-	public static final String HIBERNATE_CONF_PATH = DaoConstants.TOMCAT_HIBERNATE_LOCATION_PATH;
+	public static final String GIT_REPO_PATH = ECLIPSE_UI_GIT_REPO_PATH;
+	public static final String HIBERNATE_CONF_PATH = DaoConstants.ECLIPSE_UI_HIBERNATE_LOCATION_PATH;
 }
