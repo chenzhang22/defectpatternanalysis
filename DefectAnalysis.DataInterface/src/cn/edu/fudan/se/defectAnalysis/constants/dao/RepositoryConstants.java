4755ed857987aa67d8abac9e4e39f2208921c082
lotay <158481612@qq.com>
2014-11-10 21:05:51 +0800

    add repo constants

---
+++ DefectAnalysis.DataInterface/src/cn/edu/fudan/se/defectAnalysis/constants/dao/RepositoryConstants.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package cn.edu.fudan.se.defectAnalysis.constants.dao;
+
+/**
+ * @author Lotay
+ *
+ */
+public class RepositoryConstants {
+	public static final String ECLIPSE_CORE_REPO_PATH = "E:/Codes/libs/Eclipse/eclipse.jdt.core/.git";
+	public static final String ECLIPSE_DEBUG_REPO_PATH = "E:/Codes/libs/Eclipse/eclipse.jdt.debug/.git";
+	public static final String ECLIPSE_UI_REPO_PATH = "E:/Codes/libs/Eclipse/eclipse.jdt.ui/.git";
+	public static final String TOMCAT_REPO_PATH = "E:/Codes/libs/tomcat/.git";
+}
