c144c585d9e51f0d72cd331e9060525ef7435041
lotay <158481612@qq.com>
2014-11-10 21:06:14 +0800

    add bugInduceblameline dao

---
+++ DefectAnalysis.DataInterface/src/cn/edu/fudan/se/defectAnalysis/dao/track/BugInduceBlameLineDao.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package cn.edu.fudan.se.defectAnalysis.dao.track;
+
+import java.util.List;
+
+import cn.edu.fudan.se.defectAnalysis.bean.track.BugInduceBlameLine;
+import cn.edu.fudan.se.defectAnalysis.constants.dao.DaoConstants;
+import cn.edu.fudan.se.utils.hibernate.HibernateUtils;
+
+/**
+ * @author Lotay
+ *
+ */
+public class BugInduceBlameLineDao {
+	@SuppressWarnings("unchecked")
+	public List<BugInduceBlameLine> blameLinesForFile(String hbmConf,
+			String fileName) {
+		if (hbmConf == null || fileName == null)
+			return null;
+		String hql = "from BugInduceBlameLine where fileName='" + fileName
+				+ "' order by bugId,inducedRevisionId,inducedlineNumber";
+		return HibernateUtils.retrieveObjects(hql, hbmConf);
+	}
+
+	public static void main(String args[]) {
+		String fileName = "org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/lookup/MethodVerifier.java";
+		List<BugInduceBlameLine> lines = new BugInduceBlameLineDao()
+				.blameLinesForFile(
+						DaoConstants.ECLIPSE_CORE_HIBERNATE_LOCATION_PATH,
+						fileName);
+		System.out.println(lines);
+	}
+}
