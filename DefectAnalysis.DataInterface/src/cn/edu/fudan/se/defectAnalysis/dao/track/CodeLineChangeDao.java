25ea93767ad3ade9ea5801f8efc0b58fd33de3b9
lotay <158481612@qq.com>
2014-11-13 14:16:38 +0800

    add CodeLineChangeDao

---
+++ DefectAnalysis.DataInterface/src/cn/edu/fudan/se/defectAnalysis/dao/track/CodeLineChangeDao.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package cn.edu.fudan.se.defectAnalysis.dao.track;
+
+import java.util.List;
+
+import cn.edu.fudan.se.defectAnalysis.bean.track.CodeLineChange;
+import cn.edu.fudan.se.defectAnalysis.constants.dao.DaoConstants;
+import cn.edu.fudan.se.utils.hibernate.HibernateUtils;
+
+/**
+ * @author Lotay
+ *
+ */
+public class CodeLineChangeDao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String fileName = "org.eclipse.jdt.core/model/org/eclipse/jdt/core/IField.java", revisionId = "002377a0c081bad20cf24753616b65ca91f33618";
+		CodeLineChangeDao dao = new CodeLineChangeDao();
+		List<CodeLineChange> changeLines = dao.loadChangeLines(
+				DaoConstants.ECLIPSE_CORE_HIBERNATE_LOCATION_PATH, fileName,
+				revisionId);
+		System.out.println(changeLines);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<CodeLineChange> loadChangeLines(String hbmConf,
+			String fileName, String revisionId) {
+		if (hbmConf == null || fileName == null || revisionId == null)
+			return null;
+		String hql = "from CodeLineChange where fileName='" + fileName
+				+ "' and revisionId = '" + revisionId + "' order by lineNumber";
+		return HibernateUtils.retrieveObjects(hql, hbmConf);
+	}
+}
