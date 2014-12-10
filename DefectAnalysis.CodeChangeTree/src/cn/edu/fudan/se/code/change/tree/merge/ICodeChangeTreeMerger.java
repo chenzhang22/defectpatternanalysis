af34a050af49027e9301395bc67f197f79c92b89
lotay <158481612@qq.com>
2014-12-10 14:48:31 +0800

    add the Delete change type location extraction

--- DefectAnalysis.CodeChangeTree/src/cn/edu/fudan/se/code/change/tree/merge/ICodeChangeTreeMerger.java
+++ DefectAnalysis.CodeChangeTree/src/cn/edu/fudan/se/code/change/tree/merge/ICodeChangeTreeMerger.java
@@ -3,9 +3,12 @@
  */
 package cn.edu.fudan.se.code.change.tree.merge;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Stack;
 
+import ch.uzh.ifi.seal.changedistiller.model.entities.Delete;
 import ch.uzh.ifi.seal.changedistiller.model.entities.SourceCodeChange;
 import cn.edu.fudan.se.code.change.tree.bean.CodeChangeTreeNode;
 import cn.edu.fudan.se.code.change.tree.bean.CodeTreeNode;
@@ -27,16 +30,35 @@ public abstract class ICodeChangeTreeMerger {
 	 *            -- false: ignore delete, true: does not ignore
 	 *            delete(included).
 	 */
+
 	protected void buildChangeTreeNodeMap(CodeTreeNode codeTreeNode) {
 		changeTreeNodeMaps.clear();
+		buildChangeTreeNodeLocation(codeTreeNode,0);
+		System.out.println(changeLocations);
+	}
+	
+	private Stack<Integer> locationStack = new Stack<Integer>();
+	protected HashMap<SourceCodeChange,List<Integer>> changeLocations = new HashMap<SourceCodeChange,List<Integer>>();
+	private void buildChangeTreeNodeLocation(CodeTreeNode codeTreeNode,int index) {
+		locationStack.push(index);
 		if (codeTreeNode instanceof CodeChangeTreeNode) {
 			CodeChangeTreeNode codeChangeTreeNode = (CodeChangeTreeNode) codeTreeNode;
 			SourceCodeChange change = codeChangeTreeNode.getSourceCodeChange();
 			changeTreeNodeMaps.put(change, codeChangeTreeNode);
+			if(change instanceof Delete){
+				if(!changeLocations.containsKey(change)){
+					List<Integer> locations = new ArrayList<Integer>();
+					locations.addAll(locationStack);
+					changeLocations.put(change, locations);
+				}
+			}
 		}
 		List<CodeTreeNode> childrenNodes = codeTreeNode.getChildren();
+		int i=0;
 		for (CodeTreeNode childNode : childrenNodes) {
-			this.buildChangeTreeNodeMap(childNode);
+			buildChangeTreeNodeLocation(childNode,i);
+			i++;
 		}
+		locationStack.pop();
 	}
 }
