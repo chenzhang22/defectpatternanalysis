c298f2fb6e8421e7bcc051262cc06e9e17b86043
lotay <158481612@qq.com>
2014-12-30 18:41:55 +0800

    add BuggyTreeSpliter

---
+++ DefectAnalysis.CodeChangeTree/src/cn/edu/fudan/se/code/change/tree/split/BuggyTreeSpliter.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package cn.edu.fudan.se.code.change.tree.split;
+
+import cn.edu.fudan.se.code.change.tree.bean.CodeTreeNode;
+
+/**
+ * @author Lotay
+ *
+ */
+public abstract class BuggyTreeSpliter {
+	public abstract void split(CodeTreeNode treeNode);
+}
