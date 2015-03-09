023c6cb2a2276303ed3742129579cf584906a033
lotay <158481612@qq.com>
2015-03-09 15:35:14 +0800

    add CodeChangeTreeNodeSimilarityImpl for codechange treenode..
    Signed-off-by: lotay <158481612@qq.com>

--- DefectAnalysis.TreePatternMiner/src/cn/edu/fudan/se/tree/pattern/similarility/CodeChangeTreeNodeSimilarityImpl.java
+++ DefectAnalysis.TreePatternMiner/src/cn/edu/fudan/se/tree/pattern/similarility/CodeChangeTreeNodeSimilarityImpl.java
@@ -3,6 +3,13 @@
  */
 package cn.edu.fudan.se.tree.pattern.similarility;
 
+import org.eclipse.jdt.core.dom.ASTNode;
+
+import ch.uzh.ifi.seal.changedistiller.model.entities.Delete;
+import ch.uzh.ifi.seal.changedistiller.model.entities.Insert;
+import ch.uzh.ifi.seal.changedistiller.model.entities.Move;
+import ch.uzh.ifi.seal.changedistiller.model.entities.SourceCodeChange;
+import ch.uzh.ifi.seal.changedistiller.model.entities.Update;
 import cn.edu.fudan.se.code.change.tree.bean.CodeChangeTreeNode;
 import cn.edu.fudan.se.code.change.tree.bean.CodeTreeNode;
 
@@ -22,8 +29,47 @@ public class CodeChangeTreeNodeSimilarityImpl extends
 		}
 		CodeChangeTreeNode changeTreeNode1 = (CodeChangeTreeNode) aggreTreeNode1;
 		CodeChangeTreeNode changeTreeNode2 = (CodeChangeTreeNode) aggreTreeNode2;
+		SourceCodeChange sourceCodeChange1 = changeTreeNode1
+				.getSourceCodeChange();
+		SourceCodeChange sourceCodeChange2 = changeTreeNode2
+				.getSourceCodeChange();
+
+		Class<? extends SourceCodeChange> sourceCodeClass1 = sourceCodeChange1
+				.getClass();
+		Class<? extends SourceCodeChange> sourceCodeClass2 = sourceCodeChange2
+				.getClass();
+		ASTNode preAstNode1 = changeTreeNode1.getPreNode();
+		ASTNode preAstNode2 = changeTreeNode2.getPreNode();
+		ASTNode postAstNode1 = changeTreeNode1.getNode();
+		ASTNode postAstNode2 = changeTreeNode2.getNode();
+		if (sourceCodeClass1.equals(sourceCodeClass2)) {
+			if (sourceCodeChange1 instanceof Insert) {
+				// Insert statement/node.
+				if (postAstNode1 != null && postAstNode2 != null) {
+					return super.similarity(postAstNode1, postAstNode2);
+				}
+			} else if (sourceCodeChange1 instanceof Update
+					|| sourceCodeChange1 instanceof Move) {
+				// update or move...
+				if ((postAstNode1 != null && postAstNode2 != null)
+						&& (preAstNode1 != null && preAstNode2 != null)) {
+					return super.similarity(preAstNode1, preAstNode2)
+							* super.similarity(postAstNode1, postAstNode2);
+				}
+			} else if (sourceCodeChange1 instanceof Delete) {
+				// Delete statement/node..
+				if (preAstNode1 != null && preAstNode2 != null) {
+					return super.similarity(preAstNode1, preAstNode2);
+				}
+			}
+		}
 
 		return 0;
+	}
+
+	public static void main(String args[]) {
+		SourceCodeChange sourceCodeChange = new Insert(null, null, null);
+		System.out.println(sourceCodeChange.getClass());
 	}
 
 }
