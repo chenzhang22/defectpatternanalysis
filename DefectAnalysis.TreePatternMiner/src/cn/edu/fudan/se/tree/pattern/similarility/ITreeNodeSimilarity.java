73b125ef3c509dd206505d834081c149effc6aff
lotay <158481612@qq.com>
2015-03-20 17:51:13 +0800

    change the interface
    Signed-off-by: lotay <158481612@qq.com>

--- DefectAnalysis.TreePatternMiner/src/cn/edu/fudan/se/tree/pattern/similarility/ITreeNodeSimilarity.java
+++ DefectAnalysis.TreePatternMiner/src/cn/edu/fudan/se/tree/pattern/similarility/ITreeNodeSimilarity.java
@@ -10,7 +10,7 @@ import cn.edu.fudan.se.code.change.tree.bean.TreeNode;
  *
  */
 public interface ITreeNodeSimilarity {
-	public double similarity(TreeNode aggreTreeNode1,
-			TreeNode aggreTreeNode2);
+	public double similarity(TreeNode treeNode1,
+			TreeNode treeNode2);
 	public double treeNodeSimilarity(TreeNode node1, TreeNode node2);
 }
