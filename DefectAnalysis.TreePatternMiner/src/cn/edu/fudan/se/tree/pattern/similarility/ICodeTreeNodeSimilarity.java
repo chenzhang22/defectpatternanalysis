4f740a33c0e0c55f11e754e8fd12a24f64272f57
lotay <158481612@qq.com>
2015-03-25 10:36:26 +0800

    remove
    Signed-off-by: lotay <158481612@qq.com>

--- DefectAnalysis.TreePatternMiner/src/cn/edu/fudan/se/tree/pattern/similarility/ICodeTreeNodeSimilarity.java
+++
@@ -1,9 +0,0 @@
-package cn.edu.fudan.se.tree.pattern.similarility;
-
-import cn.edu.fudan.se.code.change.tree.bean.TreeNode;
-
-public interface ICodeTreeNodeSimilarity extends ITreeNodeSimilarity{
-	public double similarity(TreeNode node1, TreeNode node2);
-
-	public double treeNodeSimilarity(TreeNode node1, TreeNode node2);
-}
