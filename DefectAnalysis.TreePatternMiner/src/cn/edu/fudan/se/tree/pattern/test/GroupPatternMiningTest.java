1a1e73f235b0a515509db51f067850c0adf73b54
lotay <158481612@qq.com>
2015-03-25 10:36:10 +0800

    add TreeNodeTestClone and Mining Test
    Signed-off-by: lotay <158481612@qq.com>

---
+++ DefectAnalysis.TreePatternMiner/src/cn/edu/fudan/se/tree/pattern/test/GroupPatternMiningTest.java
@@ -0,0 +1,85 @@
+package cn.edu.fudan.se.tree.pattern.test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import cn.edu.fudan.se.code.change.tree.bean.TreeNode;
+import cn.edu.fudan.se.tree.pattern.mining.AbsTreeNodePatternMiner;
+import cn.edu.fudan.se.tree.pattern.mining.CodeTreeNodePatternMinerImpl;
+
+public class GroupPatternMiningTest {
+	public static void main(String args[]) {
+		TreeNodeTest buildTree1 = buildTree("1");
+		TreeNodeTest buildTree2 = buildTree("2");
+		TreeNodeTest buildTree3 = buildTree("3");
+		TreeNodeTest buildTree4 = buildTree("4");
+		TreeNodeTest buildTree5 = buildTree("5");
+		TreeNodeTest buildTree6 = buildTree("6");
+		TreeNodeTest buildTree7 = buildTree("7");
+		buildTree1.addChild(buildTree3);
+		buildTree3.addChild(buildTree7);
+		buildTree1.addChild(buildTree2);
+		buildTree2.addChild(buildTree4);
+		buildTree2.addChild(buildTree5);
+		buildTree2.addChild(buildTree6);
+
+		TreeNodeTest tree0 = buildTree("0");
+		TreeNodeTest tree1 = buildTree("1");
+		TreeNodeTest tree2 = buildTree("2");
+		TreeNodeTest tree3 = buildTree("3");
+		TreeNodeTest tree33 = buildTree("3");
+		TreeNodeTest tree4 = buildTree("4");
+		TreeNodeTest tree5 = buildTree("5");
+		TreeNodeTest tree6 = buildTree("6");
+		TreeNodeTest tree7 = buildTree("7");
+		TreeNodeTest tree8 = buildTree("8");
+		TreeNodeTest tree9 = buildTree("9");
+		TreeNodeTest tree10 = buildTree("10");
+		TreeNodeTest tree22 = buildTree("2");
+		tree0.addChild(tree1);
+		tree1.addChild(tree8);
+		tree1.addChild(tree22);
+		tree1.addChild(tree2);
+		tree1.addChild(tree33);
+
+		tree2.addChild(tree4);
+		tree4.addChild(tree10);
+		tree2.addChild(tree5);
+		tree2.addChild(tree6);
+		tree33.addChild(tree3);
+		tree3.addChild(tree9);
+		tree9.addChild(tree7);
+
+		// ImplSinglePatternInstanceMatching singlePatternInstanceMatching = new
+		// ImplSinglePatternInstanceMatching(
+		// buildTree1, new TreeNodeTestSimilarity());
+		//
+		// Map<TreeNode, TreeNode> patternMatch = singlePatternInstanceMatching
+		// .patternMatch(buildTree1, tree0);
+		// System.out.println(patternMatch);
+		// patternMatch = singlePatternInstanceMatching.patternMatch(buildTree2,
+		// tree0);
+		// System.out.println(patternMatch);
+
+		List<TreeNode> trees = new ArrayList<TreeNode>();
+		trees.add(buildTree1);
+		trees.add(tree0);
+
+		AbsTreeNodePatternMiner patternMiner = new CodeTreeNodePatternMinerImpl(
+				new TreeNodeTestSimilarity());
+		patternMiner.setTreeNodeClone(new TreeNodeTestClone());
+		Map<List<TreeNode>, Map<TreeNode, List<TreeNode>>> patterns = patternMiner
+				.mine(trees);
+		System.out.println(patterns);
+	}
+
+	/**
+	 * 
+	 */
+	private static TreeNodeTest buildTree(String id) {
+		TreeNodeTest patterNodeTest = new TreeNodeTest();
+		patterNodeTest.setNodeId(id);
+		return patterNodeTest;
+	}
+}
---
+++ DefectAnalysis.TreePatternMiner/src/cn/edu/fudan/se/tree/pattern/test/TreeNodeTestClone.java
@@ -0,0 +1,51 @@
+package cn.edu.fudan.se.tree.pattern.test;
+
+import java.util.HashMap;
+import java.util.List;
+
+import cn.edu.fudan.se.code.change.tree.bean.TreeNode;
+import cn.edu.fudan.se.code.change.tree.utils.ITreeNodeClone;
+
+@SuppressWarnings("unchecked")
+public class TreeNodeTestClone implements ITreeNodeClone {
+
+	@Override
+	public TreeNode cloneNoChildren(TreeNode codeTreeNode) {
+		if (codeTreeNode instanceof TreeNodeTest) {
+			TreeNodeTest clonedNodeTest = new TreeNodeTest();
+			clonedNodeTest.setNodeId(((TreeNodeTest) codeTreeNode).getNodeId());
+			return clonedNodeTest;
+		}
+		return null;
+	}
+
+	@Override
+	public TreeNode clone(TreeNode codeTreeNode) {
+		TreeNode clonedNode = this.cloneNoChildren(codeTreeNode);
+		if (clonedNode != null) {
+			for (TreeNode childNode : (List<TreeNode>) codeTreeNode
+					.getChildren()) {
+				clonedNode.addChild(childNode);
+			}
+		}
+		return clonedNode;
+	}
+
+	@Override
+	public TreeNode cloneWholeTree(TreeNode codeTreeNode,
+			HashMap<TreeNode, TreeNode> clonedMappedNodes) {
+		TreeNode clonedNode = this.cloneNoChildren(codeTreeNode);
+		clonedMappedNodes.put(clonedNode, codeTreeNode);
+		if (clonedNode != null) {
+			for (TreeNode childNode : (List<TreeNode>) codeTreeNode
+					.getChildren()) {
+				TreeNode clonedSubTreeNode = this.cloneWholeTree(childNode,
+						clonedMappedNodes);
+				if (clonedSubTreeNode != null) {
+					clonedNode.addChild(clonedSubTreeNode);
+				}
+			}
+		}
+		return clonedNode;
+	}
+}
