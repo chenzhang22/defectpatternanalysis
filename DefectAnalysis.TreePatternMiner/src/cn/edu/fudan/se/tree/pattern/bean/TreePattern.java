43068e9716833c99d25c49fd1c8a0758f10f3fa8
lotay <158481612@qq.com>
2015-03-27 13:27:21 +0800

    data structure for tree mining
    Signed-off-by: lotay <158481612@qq.com>

---
+++ DefectAnalysis.TreePatternMiner/src/cn/edu/fudan/se/tree/pattern/bean/TreePattern.java
@@ -0,0 +1,81 @@
+/**
+ * 
+ */
+package cn.edu.fudan.se.tree.pattern.bean;
+
+import cn.edu.fudan.se.code.change.tree.bean.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Lotay
+ *
+ */
+public class TreePattern {
+	/**
+	 * The treePattern is the pattern, which is a list of TreeNode.
+	 * 
+	 * The patternInstances is the instances containing the treePattern, the key
+	 * of patternInstances is the Tree instance, the value is a list of matched
+	 * node for the instance, each one is PatternInstanceNode.
+	 * */
+	private List<TreeNode> treePatterns = new ArrayList<TreeNode>();
+	private Map<TreeNode, List<TreePatternInstance>> patternInstances = new HashMap<TreeNode, List<TreePatternInstance>>();
+
+	public List<TreeNode> getTreePatterns() {
+		return treePatterns;
+	}
+
+	public void setTreePatterns(List<TreeNode> treePatterns) {
+		this.treePatterns = treePatterns;
+	}
+
+	public Map<TreeNode, List<TreePatternInstance>> getPatternInstances() {
+		return patternInstances;
+	}
+
+	public void setPatternInstances(
+			Map<TreeNode, List<TreePatternInstance>> patternInstances) {
+		this.patternInstances = patternInstances;
+	}
+
+	public void addTreePattern(TreeNode patternNode) {
+		this.treePatterns.add(patternNode);
+	}
+
+	public void addTreePatterns(Collection<TreeNode> patternNodes) {
+		this.treePatterns.addAll(patternNodes);
+	}
+
+	public void addPatternInstances(TreeNode instanceTree,
+			List<TreePatternInstance> treePatternInstances) {
+		this.patternInstances.put(instanceTree, treePatternInstances);
+	}
+	
+	public int getFrequency() {
+		return this.patternInstances.size();
+	}
+
+	public void addPatternInstance(TreeNode instanceTree,
+			TreePatternInstance treePatternInstance) {
+		if (instanceTree != null) {
+			List<TreePatternInstance> instanceNodes = this.patternInstances
+					.get(instanceTree);
+			if (instanceNodes == null) {
+				instanceNodes = new ArrayList<TreePatternInstance>();
+				this.patternInstances.put(instanceTree, instanceNodes);
+			}
+			instanceNodes.add(treePatternInstance);
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "TreePattern [treePatterns=" + treePatterns
+				+ ", patternInstances=" + patternInstances + "]";
+	}
+}
